c4c88ea5fea59f3d6d8b0a807c1632915b0ea761
Mihail Dmitriev <mmxdmv@mail.ru>
2023-01-21 18:10:30 +0400

    :wrap-color

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// pointerdown, pointerup, pointermove
+const debug = false;
 
 const source = document.getElementById('source');
 const move = document.getElementById('move');
 const dest1 = document.getElementById('dest1');
 const dest2 = document.getElementById('dest2');
-const debug = true;
+
 let blockCounter = 0; // Счётчик блоков
 let blockOver = 0; // Номеб блока в который кидаем
-
 let posX = 0; // Позиция начального смещения по X и Y
 let posY = 0;
 
 // Событие-перемещалка (на body, потому что ездить может везде, даже за экраном)
 const moveListener = function (event) {
     // eslint-disable-next-line no-use-before-define
-    !event.pressure && drop(event); // если отпустили за экраном
+    !event.pressure && dropListener(event); // если отпустили за экраном
     move.style.left = `${event.pageX - posX}px`;
     move.style.top = `${event.pageY - posY}px`;
 };
@@ -22,22 +21,21 @@ const moveListener = function (event) {
 const destListener = function (event) {
     if (event.type === 'pointerout') {
         blockOver = 0;
-        event.target.classList.remove('over');
+        event.target.parentElement.classList.remove('over');
     } else {
         if (event.target.id === 'dest1' || event.target.parentElement.id === 'dest1') {
             blockOver = 1;
-            document.getElementById('dest1').classList.add('over');
+            document.getElementById('dest1').parentElement.classList.add('over');
         }
         if (event.target.id === 'dest2' || event.target.parentElement.id === 'dest2') {
             blockOver = 2;
-            document.getElementById('dest2').classList.add('over');
+            document.getElementById('dest2').parentElement.classList.add('over');
         }
     }
     debug && console.log(event.type, event.target, blockOver);
-    // debug && console.log('***', event.type, event.target);
 };
 
-// Схватить (событие постоянно висит)
+// Событие хватание блока
 source.addEventListener('pointerdown', (event) => {
     // pageX pageY - абсолютные координаты блока
     // offsetX offsetY - относительные координаты клика внутри блока
@@ -62,7 +60,7 @@ source.addEventListener('pointerdown', (event) => {
 });
 
 // Событие при опускании
-const drop = function (event) {
+const dropListener = function (event) {
     // Скрыли таскалку
     move.classList.add('hidden');
 
@@ -76,8 +74,8 @@ const drop = function (event) {
     debug && console.log('Кинули: ', event.pageX, event.pageY);
     if (blockOver) {
         const blockTarget = blockOver === 1 ? dest1 : dest2;
-        blockTarget.classList.remove('over');
-        debug && console.log('Попали: ', blockTarget);
+        blockTarget.parentElement.classList.remove('over');
+        debug && console.log('Попали: ', blockTarget.parentElement);
 
         const elem = document.createElement('div'); // элемент для вставки
         // eslint-disable-next-line no-plusplus
@@ -85,15 +83,8 @@ const drop = function (event) {
         elem.style.backgroundColor = move.style.backgroundColor;
         blockTarget.append(elem);
 
-        if (blockTarget === dest1) {
-            // Костыль по установке высоты элементу (фону)
-            if (dest1.children.length > 9) {
-                // 101 - высота блока, 3 - кол-во блоков в строке
-                const newHeight = 101 * (1 + parseInt(dest1.children.length / 3, 10));
-                console.log(dest1.children.length, newHeight);
-                document.styleSheets[0].cssRules[0].style.height = `${newHeight}px`;
-            }
-        } else {
+        if (blockTarget === dest2) {
+            // определяем относительные координаты top = y, left = x, если координаты отрицательные то зануляем их, а отрицательность превращаем в дельту
             let top = dest2.scrollTop + event.layerY - posY; // Отступ сверху
             const deltaTop = top < 0 ? -top : 0;
             top = top < 0 ? 0 : top;
@@ -102,23 +93,20 @@ const drop = function (event) {
             left = left < 0 ? 0 : left;
 
             if (deltaTop + deltaLeft) {
+                // Если вылезли за левый верхний край, то двигаем ВСЕ внутренние блоки на дельту вправо-вниз
                 for (const block of dest2.children) {
                     block.style.top = parseInt(block.style.top, 10) + deltaTop;
                     block.style.left = parseInt(block.style.left, 10) + deltaLeft;
-                    console.log(block.style.top, block.style.left);
                 }
-                dest2.scrollTop = deltaTop;
-                dest2.scrollLeft = deltaLeft;
+                dest2.scrollTop = parseInt(dest2.scrollTop, 10) + deltaTop;
+                dest2.scrollLeft = parseInt(dest2.scrollLeft, 10) + deltaLeft;
             }
-
             elem.style.top = top;
             elem.style.left = left;
-            console.log(top, left, deltaTop, deltaLeft);
         }
-
         blockOver = 0; // обнулили блок
     }
 };
 
 // Отпустить (событие постоянно висит)
-move.addEventListener('pointerup', drop);
+move.addEventListener('pointerup', dropListener);

03fd5126c55fbbadc10a0586729aae1aa9d6cf90
Mihail Dmitriev <mmxdmv@mail.ru>
2023-01-21 15:18:45 +0400

    :auto-scroll

--- index.js
+++ index.js
@@ -19,27 +19,22 @@ const moveListener = function (event) {
     move.style.top = `${event.pageY - posY}px`;
 };
 
-// Событие-определялка куда кинули
-const dropListener = function (event) {
-    // Событие одноразовое, прибиваем
-    console.log('Попал', event.target.id);
-    const elem = document.createElement('div'); // элемент для вставки
-    // eslint-disable-next-line no-plusplus
-    elem.innerHTML = `Блок номер ${++blockCounter}`;
-    event.target.append(elem);
-    console.log(event.target);
-};
-
 const destListener = function (event) {
-    // eslint-disable-next-line no-nested-ternary
-    blockOver = event.type === 'pointerout' ? 0 : event.target.id === 'dest1' ? 1 : 2;
-    if (blockOver) {
-        event.target.classList.add('over');
-    } else {
+    if (event.type === 'pointerout') {
+        blockOver = 0;
         event.target.classList.remove('over');
+    } else {
+        if (event.target.id === 'dest1' || event.target.parentElement.id === 'dest1') {
+            blockOver = 1;
+            document.getElementById('dest1').classList.add('over');
+        }
+        if (event.target.id === 'dest2' || event.target.parentElement.id === 'dest2') {
+            blockOver = 2;
+            document.getElementById('dest2').classList.add('over');
+        }
     }
     debug && console.log(event.type, event.target, blockOver);
-    return false;
+    // debug && console.log('***', event.type, event.target);
 };
 
 // Схватить (событие постоянно висит)
@@ -58,31 +53,71 @@ source.addEventListener('pointerdown', (event) => {
     move.classList.remove('hidden');
     debug && console.log('Схватили:', event.pageX, event.pageY, randColor);
 
-    document.body.addEventListener('pointermove', moveListener); // Для движения
-    dest1.addEventListener('pointerover', destListener); // Для наведения
+    document.body.addEventListener('pointermove', moveListener); // Движение
+    // контроль попадания
+    dest1.addEventListener('pointerover', destListener); // Наведение
     dest2.addEventListener('pointerover', destListener);
-    dest1.addEventListener('pointerout', destListener); // Для снятия
+    dest1.addEventListener('pointerout', destListener); // Снятие
     dest2.addEventListener('pointerout', destListener);
-
 });
 
 // Событие при опускании
 const drop = function (event) {
     // Скрыли таскалку
     move.classList.add('hidden');
-    // Проверяем куда кинули
-    dest1.addEventListener('pointermove', dropListener);
-    dest2.addEventListener('pointermove', dropListener);
 
-    setTimeout(() => {
-        // через минимальное время отключаем проверялку для исключения ложных попаданий
-        dest1.removeEventListener('pointermove', dropListener);
-        dest2.removeEventListener('pointermove', dropListener);
-    }, 100);
-
-    // Отключили перемещалку
+    // перемещение
     document.body.removeEventListener('pointermove', moveListener);
+    // контроль попадания
+    dest1.removeEventListener('pointerover', destListener); // Наведение
+    dest2.removeEventListener('pointerover', destListener);
+    dest1.removeEventListener('pointerout', destListener); // Снятие
+    dest2.removeEventListener('pointerout', destListener);
     debug && console.log('Кинули: ', event.pageX, event.pageY);
+    if (blockOver) {
+        const blockTarget = blockOver === 1 ? dest1 : dest2;
+        blockTarget.classList.remove('over');
+        debug && console.log('Попали: ', blockTarget);
+
+        const elem = document.createElement('div'); // элемент для вставки
+        // eslint-disable-next-line no-plusplus
+        elem.innerHTML = `Элемент номер ${++blockCounter}`;
+        elem.style.backgroundColor = move.style.backgroundColor;
+        blockTarget.append(elem);
+
+        if (blockTarget === dest1) {
+            // Костыль по установке высоты элементу (фону)
+            if (dest1.children.length > 9) {
+                // 101 - высота блока, 3 - кол-во блоков в строке
+                const newHeight = 101 * (1 + parseInt(dest1.children.length / 3, 10));
+                console.log(dest1.children.length, newHeight);
+                document.styleSheets[0].cssRules[0].style.height = `${newHeight}px`;
+            }
+        } else {
+            let top = dest2.scrollTop + event.layerY - posY; // Отступ сверху
+            const deltaTop = top < 0 ? -top : 0;
+            top = top < 0 ? 0 : top;
+            let left = dest2.scrollLeft + event.layerX - posX; // Отступ слева
+            const deltaLeft = left < 0 ? -left : 0;
+            left = left < 0 ? 0 : left;
+
+            if (deltaTop + deltaLeft) {
+                for (const block of dest2.children) {
+                    block.style.top = parseInt(block.style.top, 10) + deltaTop;
+                    block.style.left = parseInt(block.style.left, 10) + deltaLeft;
+                    console.log(block.style.top, block.style.left);
+                }
+                dest2.scrollTop = deltaTop;
+                dest2.scrollLeft = deltaLeft;
+            }
+
+            elem.style.top = top;
+            elem.style.left = left;
+            console.log(top, left, deltaTop, deltaLeft);
+        }
+
+        blockOver = 0; // обнулили блок
+    }
 };
 
 // Отпустить (событие постоянно висит)
